ae3d7526dd2b3a406ada1a42b1ffe7477ef00d65
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-09-16 20:24:35 +0200

    fix error home Video

--- src/app/component/HomeBackground.tsx
+++ src/app/component/HomeBackground.tsx
@@ -4,16 +4,16 @@ import React from "react";
 const HomeBackground = () => {
   return (
     <div
-  className='w-full h-screen relative before:content-[""] before:absolute before:top-0 
-  before:left-0 before:w-full before:h-full before:z-10 before:opacity-50 
-  after:content-[""] after:absolute after:left-0 after:right-0 after:bottom-0 after:h-full after:bg-gradient-to-t after:from-black after:to-transparent'
->
+      className='w-full h-screen relative overflow-hidden before:content-[""] before:absolute before:top-0 
+    before:left-0 before:w-full before:h-full z-10 after:content-[""] after:absolute 
+    after:left-0 after:right-0 after:-bottom-px after:h-full 
+    '
+    >
       <video
         className="absolute top-1/2 left-1/2 w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 -z-10"
         autoPlay
         loop
         muted
-        playsInline poster="/home-video.mp4"
       >
         <source src="/home-video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
