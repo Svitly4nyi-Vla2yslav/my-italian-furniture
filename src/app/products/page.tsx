32e3a7beb1b7011bc1abb4972c282c4f09ffc60e
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2025-01-21 22:26:56 +0100

    blur / transparent

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -9,27 +9,27 @@ const Products: React.FC = () => {
       <p className='text-lg text-center text-gray-600'>Ми пропонуємо високоякісні італійські меблі.</p>
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 h-screen">
           {/* Карточки продуктів */}
-          <div className="bg-slate-500 p-6 rounded-lg shadow-md">
+          <div className="bg-transparent backdrop-blur-xl p-6 rounded-lg shadow-md">
             <h3 className="text-xl font-bold mb-2"> Product №1</h3>
             <p className="text-primary"> About product</p>
           </div>
-          <div className="bg-slate-500 p-6 rounded-lg shadow-md">
+          <div className="bg-transparent backdrop-blur-xl p-6 rounded-lg shadow-md">
             <h3 className="text-xl font-bold mb-2"> Product №1</h3>
             <p className="text-primary"> About product</p>
           </div>
-          <div className="bg-slate-500 p-6 rounded-lg shadow-md">
+          <div className="bg-transparent backdrop-blur-xl p-6 rounded-lg shadow-md">
             <h3 className="text-xl font-bold mb-2"> Product №1</h3>
             <p className="text-primary"> About product</p>
           </div>
-          <div className="bg-slate-500 p-6 rounded-lg shadow-md">
+          <div className="bg-transparent backdrop-blur-xl p-6 rounded-lg shadow-md">
             <h3 className="text-xl font-bold mb-2"> Product №1</h3>
             <p className="text-primary"> About product</p>
           </div>
-          <div className="bg-slate-500 p-6 rounded-lg shadow-md">
+          <div className="bg-transparent backdrop-blur-xl p-6 rounded-lg shadow-md">
             <h3 className="text-xl font-bold mb-2"> Product №1</h3>
             <p className="text-primary"> About product</p>
           </div>
-          <div className="bg-slate-500 p-6 rounded-lg shadow-md">
+          <div className="bg-transparent backdrop-blur-xl p-6 rounded-lg shadow-md">
             <h3 className="text-xl font-bold mb-2"> Product №1</h3>
             <p className="text-primary"> About product</p>
           </div>
